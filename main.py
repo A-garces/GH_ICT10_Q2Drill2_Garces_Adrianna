b7aa25b981dd9e73c42f54e7a9ef75ded1e06e9c
A-garces <garces.adm@obmontessori.edu.ph>
2025-11-17 14:14:38 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+from pyscript import display
+
+band = {'Kelsey', 'Selena', 'Jennifer', 'Phoebe'}
+dance = {'Jennifer', 'Jade', 'Ashley', 'Erich'}
+
+display(f'Students in one of the clubs: {band | dance}', target='output')
+display(f'Students in both clubs: {band & dance}', target='output')
+display(f'Students in Band: {band - dance}', target='output')
+display(f'Students in Dance: {dance - band}', target='output')
+display(f'Students with only one clubs{band ^ dance}', target='output')
